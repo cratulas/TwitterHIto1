75bdb701c62397a8d3bc44dd454dffd6bacccadc
Mauricio Mora <mauricios.mora@gmail.com>
2021-06-16 00:29:26 -0400

    Se agrega nuevo tweet en index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,7 +6,7 @@ class HomeController < ApplicationController
         # @tweets = Tweet.page(params[:page]).per(3).order(:content)
         # @tweets = Tweet.order(:id).page(params[:page])
         # @tweets = Tweet.page(1).per(50).padding(3)
-
+        @tweet = Tweet.new
         @tweets = Tweet.page(params[:page]).per(10).order(:created_at)
     end
     
